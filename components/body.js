05b088b3570583a9cd02243d8222a44e26239012
Nishat <nishatparween095@gmail.com>
2023-07-05 16:06:50 +0530

    added body

--- components/body.js
+++ components/body.js
@@ -1,14 +1,40 @@
-import {restList} from "./restList";
+import { restList } from "./restList";
 import { RestCard } from "./card";
+import { useState } from "react";
+
+function filterData(searchInput,restList){
+  const filterData=restList.filter((restaurant) => 
+  restlist.rname.includes(searchInput)
+  );
+  return filterData;
+  }
 
 const Body= () => {
-   console.log(restList);
+
+   const [restList,setrestList]= useState(restList);
+   const [searchInput,setsearchInput]=useState();
     return(
       <>
       <div className="Search-container">
-        <input type="text" className="search-input" placeholder="Search" value="" />
-        <button className="search-btn">Search</button>
+        <input 
+        type="text" 
+        className="search-input" 
+        placeholder="Search" 
+        value= {searchInput}
+            onChange={(e) => {
+              setsearchInput(e.target.value)
+            }
+            } 
+        />
+
+        <button className="search-btn" onClick={ () => {
+          const data=filterData(searchInput,restList);
+          setrestList(data);
+
+        }}
+        >Search</button>
       </div>
+      
       <div className="resturant-list">
         {restList.map((restaurant) => {
           return < RestCard {...restaurant} />;
